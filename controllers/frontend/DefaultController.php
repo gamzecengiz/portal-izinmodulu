33c870395ed8db8594459b39a5d1e42ef017cbd0
gamzecengiz <gamzecengiz07@gmail.com>
2018-12-14 18:10:51 +0300

    first

---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\izinmodulu\controllers\frontend;
+
+
+/**
+ * Default controller for the `izinmodulu` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
